f09f6c47850e649ecfdaf8377effee5d5efcbead
Abhijeet Kumar <abhikr917@gmail.com>
2026-05-23 13:32:34 +0530

    Add assignment 6 resubmission step 3 files

--- assignments/assignment_06_module_14_15_tkinter_calculator/resubmission/calculator.py
+++ assignments/assignment_06_module_14_15_tkinter_calculator/resubmission/calculator.py
@@ -22,4 +22,25 @@ button_2.grid(row=0, column=1)
 button_3 = tk.Button(button_frame, text="3", width=5, height=2, command=lambda: click_number("3"))
 button_3.grid(row=0, column=2)
 
+button_4 = tk.Button(button_frame, text="4", width=5, height=2, command=lambda: click_number("4"))
+button_4.grid(row=1, column=0)
+
+button_5 = tk.Button(button_frame, text="5", width=5, height=2, command=lambda: click_number("5"))
+button_5.grid(row=1, column=1)
+
+button_6 = tk.Button(button_frame, text="6", width=5, height=2, command=lambda: click_number("6"))
+button_6.grid(row=1, column=2)
+
+button_7 = tk.Button(button_frame, text="7", width=5, height=2, command=lambda: click_number("7"))
+button_7.grid(row=2, column=0)
+
+button_8 = tk.Button(button_frame, text="8", width=5, height=2, command=lambda: click_number("8"))
+button_8.grid(row=2, column=1)
+
+button_9 = tk.Button(button_frame, text="9", width=5, height=2, command=lambda: click_number("9"))
+button_9.grid(row=2, column=2)
+
+button_0 = tk.Button(button_frame, text="0", width=5, height=2, command=lambda: click_number("0"))
+button_0.grid(row=3, column=1)
+
 window.mainloop()
