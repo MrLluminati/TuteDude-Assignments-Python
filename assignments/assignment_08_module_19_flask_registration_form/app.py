191344f5bbfe3add4a3d1b2a9006d6983f7bd7a3
Abhijeet Kumar <abhikr917@gmail.com>
2026-05-26 01:39:26 +0530

    Add assignment 8 simple form validation

--- assignments/assignment_08_module_19_flask_registration_form/app.py
+++ assignments/assignment_08_module_19_flask_registration_form/app.py
@@ -1,5 +1,5 @@
 # Assignment 8 - Flask Registration Form
-# Step 3: Read submitted form data using POST
+# Step 4: Add simple form validation
 
 from flask import Flask, render_template, request
 
@@ -8,17 +8,22 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    error = ""
+
     if request.method == "POST":
         name = request.form.get("name")
         email = request.form.get("email")
         course = request.form.get("course")
 
-        print("Form submitted successfully.")
-        print("Name:", name)
-        print("Email:", email)
-        print("Course:", course)
+        if name == "" or email == "" or course == "":
+            error = "All fields are required."
+        else:
+            print("Form submitted successfully.")
+            print("Name:", name)
+            print("Email:", email)
+            print("Course:", course)
 
-    return render_template("register.html")
+    return render_template("register.html", error=error)
 
 
 if __name__ == "__main__":
